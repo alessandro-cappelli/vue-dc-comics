3db6c9aa4f97f5c63ffdd2bf3d96aeece4df8687
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-01-27 17:06:51 +0100

    parte header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="container">
+      <section>
+          <div>
+              <img src="../assets/img/dc-logo.png" alt="">
+          </div>
 
+          <div>
+              <ul>
+                  <li><a href="#">CHARACTERS</a></li>
+                  <li><a href="#">COMICS</a></li>
+                  <li><a href="#">MOVIES</a></li>
+                  <li><a href="#">TV</a></li>
+                  <li><a href="#">GAMES</a></li>
+                  <li><a href="#">COLLECTIBLES</a></li>
+                  <li><a href="#">VIDEOS</a></li>
+                  <li><a href="#">FANS</a></li>
+                  <li><a href="#">NEWS</a></li>
+                  <li><a href="#">SHOP</a></li>
+              </ul>
+          </div>
+
+      </section>
   </div>
 </template>
 
